d0979fb25ebd76a6e6929c8fca1c8cbd3a149d4e
Fortune <fortunejite1234@gmail.com>
2026-01-20 22:48:21 +0100

    feat(prisma): implement global Prisma client instance for improved performance

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -2,7 +2,12 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 import config from "@/config";
 
-const adapter = new PrismaPg({ connectionString: config.databaseUrl });
-const prisma = new PrismaClient({ adapter });
+let globalForPrisma = global as unknown as { prisma?: PrismaClient, adapter?: PrismaPg };
+const adapter = globalForPrisma?.adapter ?? new PrismaPg({ connectionString: config.databaseUrl });
+const prisma = globalForPrisma?.prisma ?? new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma = { adapter, prisma }
+}
 
 export { prisma };
