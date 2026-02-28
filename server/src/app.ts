aa6d98fb13ffe221ecbe5b1a8253ba81e1e0199e
Fortune <fortunejite1234@gmail.com>
2026-02-28 04:27:35 +0100

    feat: refactor server initialization by moving app setup to a separate startApp function

---
+++ server/src/app.ts
@@ -0,0 +1,76 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import errorHandler from './middlewares/error.middleware';
+import {
+  accountRoutes,
+  assetRoutes,
+  authRoutes,
+  coinRoutes,
+  depositRoutes,
+  investmentPlanRoutes,
+  investmentRoutes,
+  marketRoutes,
+  settingsRoutes,
+  statsRoutes,
+  swapRoutes,
+  positionRoutes,
+  traderRoutes,
+  tradeSignalRoutes,
+  transactionRoutes,
+  userRoutes,
+  withdrawalRoutes,
+} from './routes';
+import { authenticate, authorize } from './middlewares/auth.middleware';
+import config from './config';
+import loggerMiddleware from './middlewares/logger.middleware';
+
+export const startApp = () => {
+  const app = express();
+
+  app.use(
+    cors({
+      origin: config.clientUrl,
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+
+  app.use(loggerMiddleware);
+
+  // Routes
+  app.use('/account', authenticate, accountRoutes);
+  app.use('/assets', authenticate, assetRoutes);
+  app.use('/auth', authRoutes);
+  app.use('/coins', coinRoutes);
+  app.use('/investment-plans', authenticate, investmentPlanRoutes);
+  app.use('/investments', authenticate, investmentRoutes);
+  app.use('/deposits', authenticate, depositRoutes);
+  app.use('/market', marketRoutes);
+  app.use('/positions', authenticate, positionRoutes);
+  app.use('/stats', authenticate, statsRoutes);
+  app.use('/traders', traderRoutes);
+  app.use('/trading-signals', authenticate, tradeSignalRoutes);
+  app.use('/transactions', authenticate, transactionRoutes);
+  app.use('/withdrawals', authenticate, withdrawalRoutes);
+  app.use('/settings', authenticate, settingsRoutes);
+  app.use('/swaps', authenticate, swapRoutes);
+  app.use('/users', authenticate, authorize(['admin']), userRoutes);
+
+  app.use('/status', (req, res) => {
+    res.status(200).json({ running: true });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not Found' });
+  });
+
+  // Error handling middleware
+  app.use(errorHandler);
+
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+  });
+};
